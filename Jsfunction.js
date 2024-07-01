022d92a1d96be9056855df73870bb4c14bbc172c
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2024-07-01 23:11:59 +0530

    Create Jsfunction.js

---
+++ Jsfunction.js
@@ -0,0 +1,18 @@
+function myfunction(a,b){
+    return a+b;
+}
+let x= myfunction(2,3);
+console.log(x);
+
+
+const add =function(a,b){
+    return a+b;
+}
+console.log(add(5,4));
+
+
+
+const squr=x=>{
+    return x*x;
+}
+console.log(squr(4));
