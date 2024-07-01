4fc9bb97b2993cd4a081ca0531dd8738b579b4f9
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2024-07-01 23:41:22 +0530

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,17 @@
+ 
+// using object literal
+const person ={name:'avinash',age:23};
+console.log(person.name);
+
+const person1={};
+person1.name="akash";
+person1.age="22";
+console.log(person1.name);
+
+
+//using new keyword
+
+const person2= new Object();
+person2.name ="kanha";
+person2.age =26;
+console.log(person2.age);
